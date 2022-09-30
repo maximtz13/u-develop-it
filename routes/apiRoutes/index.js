3fd2c71937f0da034e667ec3d2d9deeff436f7f6
Maximoo <maximtz99@gmail.com>
2022-09-29 21:59:40 -0500

    added votes table

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 router.use(require('./voterRoutes'));
 router.use(require('./candidateRoutes'));
+router.use(require('./voteRoutes'));
 
 module.exports = router;
